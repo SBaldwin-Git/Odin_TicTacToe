fffeebac086280c89a7e8805c4a703aaf0dc52c3
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-13 11:25:38 +0000

    HTML/CSS fully added, function templates created

---
+++ script.js
@@ -0,0 +1,36 @@
+const gameBoard = (() => {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return {
+    add,
+    sub,
+    mul,
+    div,
+  };
+})();
+
+const displayController = (() => {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const mul = (a, b) => a * b;
+  const div = (a, b) => a / b;
+  return {
+    add,
+    sub,
+    mul,
+    div,
+  };
+})();
+
+const playerFactory = (name, age) => {
+  const sayHello = () => console.log('hello!');
+  return {
+    name,
+    age,
+    sayHello
+  };
+};
+
+const jeff = personFactory('jeff', 27)
